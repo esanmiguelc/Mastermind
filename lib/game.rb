27f17cadcaa5a62cd61c52c9468e15d71d30daad
Emmanuel San Miguel <esanmiguel@gmail.com>
2014-05-20 21:59:45 -0400

    updating game

--- lib/game.rb
+++ lib/game.rb
@@ -7,22 +7,28 @@ class Game
     puts "Starting new game..."
     computer = Computer.new
     user = User.new
+    guesses = []
+    feedbacks = []
     guess_num = 1
     s = computer.generate_patterns
     begin
       guess = computer.current_guess(s)
+      guesses << guess
       puts "#{guess}"
       feedback = user.give_feedback
-      puts "feedback: #{feedback}, number of guesses: #{guess_num}"
+      feedbacks << feedback
+      build_board(guesses, feedbacks)
       s = computer.eliminate_codes(s, guess, feedback)
       guess_num += 1
     end while feedback[0] != 4
-    puts "#{guess}, Yay! Game over" 
+    puts "#{guess}, Yay! Game over in #{guess_num} tries" 
+  end
+
+  def build_board(guess, feedback)
+    guess.each_index do |e|
+      puts "||  #{guess[e]}  || #{feedback[e]}"
+    end
   end
 end
 
-||["R","R","R","R"]|| [2,0]
-||["R","R","O","O"]|| [4,0]
-===========================
-"Game over yay!"
 
