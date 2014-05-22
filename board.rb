3aa3644a12393b7b60b8b5bfb5dd70c6b3ec3bdb
Emmanuel San Miguel <esanmiguel@gmail.com>
2014-05-21 22:18:11 -0400

    making file runnable without irb

--- board.rb
+++ board.rb
@@ -1,9 +1,12 @@
+#!/usr/bin/env ruby
 require_relative "lib/game"
 
 puts "Welcome to a game of Mastermind, it is a codebreaking game 
 where the computer will attempt to guess a code consisting a pattern of four colors\n 
 The available colors are: 'R', 'G', 'Y', 'B', 'P', 'O', you can select any of 
-the colors and the colors can be repeated any number of times\n
+the colors and the colors can be repeated any number of times\n\n
+You will be prompted on how many colors from the computer's guess was correct
+(same color on correct position) or close (same color wrong position)\n
 Please think of a pattern\nWhen ready type: 's'"
 
 answer = gets.chomp.downcase
